59c2d923f927b4dfb4f4b02586f188215a822b60
Niyas Nazar <niyas28nazar@gmail.com>
2025-07-07 15:26:47 +0530

    sample data has been added for all the tables

--- sql/database_script.sql
+++ sql/database_script.sql
@@ -41,3 +41,34 @@ create table payments (
     payment_method varchar(50),
     foreign key(order_id) reffrences orders(order_id)
 );
+
+
+-- Sample Data: Customers:
+
+insert into customers(name,email,phone)
+values
+('Niyas','niyas28nazar@gmail.com','7907291887'),
+('Akhil','akhil007@gmial.com','8958845658');
+
+--Sample Dta:Products:
+
+insert into products(name,description,price,stock)
+values
+('Axxor','Retro style',2500.00,10),
+('Mt','Apex style',6000.00,5);
+
+
+--Sample Data :Orders:
+
+insert into orders(customer_id,order_date,status,total_amount)
+values
+(1,'2025-07-06','delivered',1500.00),
+(2, '2025-07-07', 'pending', 6000.00);
+
+
+--sample Data: Payments:
+insert into payments(order_id,payment_date,amount,payment_method,status)
+values
+(7,'2025-07-06',2500.00,'UPI','paid'),
+(8,'2025-07-07',6000.00,'Cash','failed');
+
