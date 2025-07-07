5e634cf5a62c420d207f071e0ba50bcfa983ea9e
Niyas Nazar <niyas28nazar@gmail.com>
2025-07-07 14:56:29 +0530

    tables created for customers,products,orders and payments

---
+++ sql/database_script.sql
@@ -0,0 +1,43 @@
+--Table for Customers:
+
+create table customers(
+    customers_id serial primary key,
+    name varchar(100) not null,
+    email varchar(100) unique not null,
+    phone varchar(15),
+    created_at  timestamp default current_timestamp
+);
+
+--Table for Products:
+
+create table products (
+    product_id serial primary key,
+    name varchar(100) not null,
+    description text,
+    price decimal(10,2) not null,
+    stock int default 0,
+    created_at timestamp default current_timestamp
+);
+
+--Table for Orders:
+
+create table orders(
+    oreder_id serial primary key,
+    customer_id int  not null,
+    order_date date default current_date,
+    status varchar(20) not null check(status in ('pending','shipped','delivered','cancelled')),
+    total_amount decimal(10,2),
+    foreign key (customer_id) reffrences customers(customer_id)
+);
+
+--Table for Payments:
+
+create table payments (
+    payment_id serial primary key,
+    order_id int not null,
+    payment_date date default current_date,
+    amount decimal(10,2) not null,
+    status varchar(20) check (status in ('paid', 'failed', 'refunded')),
+    payment_method varchar(50),
+    foreign key(order_id) reffrences orders(order_id)
+);
